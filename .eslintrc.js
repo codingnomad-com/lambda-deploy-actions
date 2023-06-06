34164e98ad253115e1740633a3c59fa58f9ffeb7
Manuel Bachl <manuel@coding-nomad.com>
2023-06-06 14:28:22 +0800

    feat(setup): added missing dependencies, set to typescript

---
+++ .eslintrc.js
@@ -0,0 +1,123 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-import',
+    'prettier',
+    'prefer-arrow-functions',
+  ],
+  // we're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it we have to explicitly
+  // set the jest version.
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['camelCase', 'PascalCase'],
+        selector: 'default',
+      },
+      {
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        selector: ['variable', 'function'],
+      },
+      {
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        selector: 'parameter',
+      },
+      {
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'require',
+        modifiers: ['private'],
+        selector: 'memberLike',
+      },
+      {
+        format: ['PascalCase'],
+        selector: 'typeLike',
+      },
+    ],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/extensions': 0,
+    'import/no-default-export': 0,
+    'import/no-unresolved': 'error',
+    'import/prefer-default-export': 0,
+    'jest/no-export': 'off',
+    'jsx-a11y/label-has-associated-control': 0,
+    'no-console': 1,
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-useless-constructor': 0,
+    'object-curly-newline': 'off',
+    'prefer-arrow-functions/prefer-arrow-functions': [
+      'warn',
+      {
+        classPropertiesAllowed: false,
+        disallowPrototype: false,
+        returnStyle: 'unchanged',
+        singleReturnOnly: false,
+      },
+    ],
+    'prefer-destructuring': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        bracketSameLine: false,
+        bracketSpacing: true,
+        endOfLine: 'auto',
+        jsxDoubleQuote: true,
+        overrides: [
+          {
+            files: '*.md',
+            options: {
+              parser: 'markdown',
+            },
+          },
+        ],
+        printWidth: 80,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
+    ],
+  },
+  settings: {
+    jest: {
+      version: 28,
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      // 'babel-module': {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
+};
---
+++ app/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import { APIGatewayEvent, APIGatewayProxyCallback, Context } from 'aws-lambda';
+
+export const lambdaHandler = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: APIGatewayProxyCallback,
+): void => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'hello world',
+    }),
+  });
+};
